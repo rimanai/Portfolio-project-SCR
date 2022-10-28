97c0eb92e4e88eb7cc2d06f281e3c5055056b620
rimanai <109399022+rimanai@users.noreply.github.com>
2022-10-28 16:53:23 +1100

    Polish: Menu Button
    - Move menu button to different col.
    - Add button handler for menu reopening button

--- src/js.js
+++ src/js.js
@@ -4,6 +4,14 @@ button.addEventListener("click", handleMenu);
 function handleMenu() {
   let sidebar = document.querySelector("div#side-nav");
   sidebar.style.visibility = "hidden";
-  let fakesidebar = document.querySelector("#fake-side-nav");
-  fakesidebar.style.visibility = "hidden";
+  //let fakesidebar = document.querySelector("#fake-side-nav");
+  //fakesidebar.style.visibility = "hidden";
 }
+
+function reopenMenu() {
+  let opensidebar = document.querySelector("#side-nav");
+  opensidebar.style.visibility = "initial";
+}
+
+let reopen = document.querySelector("#reopen-button");
+reopen.addEventListener("click", reopenMenu);
