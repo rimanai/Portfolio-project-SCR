e22510863461696d87b42d224e8634b2a1cddb79
rimanai <109399022+rimanai@users.noreply.github.com>
2022-10-28 16:26:45 +1100

    Fix: HTML
    - Fix bug with menu items disappearing with latency;
    
    - Polish responsiveness of the sidebar.

--- src/js.js
+++ src/js.js
@@ -4,4 +4,6 @@ button.addEventListener("click", handleMenu);
 function handleMenu() {
   let sidebar = document.querySelector("div#side-nav");
   sidebar.style.visibility = "hidden";
+  let fakesidebar = document.querySelector("#fake-side-nav");
+  fakesidebar.style.visibility = "hidden";
 }
